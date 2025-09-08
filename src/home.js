f20e46c098295433c2c6cc5bcedf488300e9f018
Sharmeen <sharmeenk2020@gmail.com>
2025-09-09 04:41:04 +0530

    Data

--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 import "./styles/home.css";
 import homeImg from "./assets/home1.png";
 import home2Img from "./assets/home2.png";
+import home3Img from "./assets/home3.png"
 
 
 function HomePage(){
@@ -27,10 +28,35 @@ function HomePage(){
     para.appendChild(h1);
     para.appendChild(h2);
     container.appendChild(para);
-    // container.appendChild(img2)
+    const box=document.createElement("p")
+    box.classList.add('box')
+    para.appendChild(box);
+    const text=document.createElement("p");
+    text.classList.add('text');
+    text.textContent="Born from tradition, rebuilt with ambition — we serve food that’s bold, seasonal, and deeply connected to Chicago.";
+    para.appendChild(text);
+    const img3=document.createElement("img");
+    img3.src=home3Img;
+    img3.classList.add('img3');
+    para.appendChild(img3);
     content.appendChild(container);
+    const para2=document.createElement("p")
+    para2.textContent="At The Bear, the food is only half the story. The people are the rest.Our crew is a family forged in fire — sometimes messy, always passionate, and committed to making something greater than themselves."
+    para2.classList.add("text1","hidden-text");
+    para.appendChild(para2);
+    const observer=new IntersectionObserver(entries=>{
+        entries.forEach(entry=>{
+            if(entry.isIntersecting){
+                entry.target.classList.add('visible');
+            }
+            else{
+                entry.target.classList.remove("visible")
+            }
+        })
+    })
 
-
+    document.querySelectorAll(".hidden-text").forEach(entry=>observer.observe(entry));
+    
 
 }
 
